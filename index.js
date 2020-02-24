4badc43aaa7adb43cfb53de6a8fd71e6788e8dd0
github username <github email address>
2020-02-24 15:44:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 // Your code here
+const mapToNegativize = (sourceArray) => {
+    let negativeArray = []
+    for (let i = 0; i < sourceArray.length; i++){
+        let negativeNum = -sourceArray[i]
+        negativeArray.push(negativeNum)
+    }
+    return negativeArray
+}
+
+const mapToNoChange = (sourceArray) => {
+    return sourceArray
+}
+
+const mapToDouble = (sourceArray) => {
+    let doubleArray = []
+    for (let i = 0; i < sourceArray.length; i++){
+        let doubleNum = sourceArray[i] * 2
+        doubleArray.push(doubleNum)
+    }
+    return doubleArray
+}
+
+const mapToSquare = (src) => {
+    let squared = []
+    for(let i = 0; i < src.length; i++){
+        let sNum = src[i] ** 2
+        squared.push(sNum)
+    }
+    return squared
+}
+
+const reduceToTotal = (sourceArray, startingPoint= 0) => {
+    let total = startingPoint
+    for(let i= 0; i < sourceArray.length; i++ ){
+        total = total + sourceArray[i]
+    }
+    return total
+}
+
+const reduceToAllTrue = (array) => {
+    let endResults = true
+    for(let i = 0; i < array.length; i++){
+        if(array[i]=== false){
+            endResults = false
+        } else{
+            endResults = true
+        }
+    }
+    return endResults
+}
+
+const reduceToAnyTrue = (array) => {
+    let endResults = true
+    for(let i = 0; i< array.length; i++){
+        if(array[i] !== true){
+            endResults = false
+        } else{
+            endResults = true
+        }
+    }
+    return endResults
+}
